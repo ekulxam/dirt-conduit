031830332ac8e555824c443d63f748115a8e6c25
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2026-02-04 03:56:38 -0800

    I only need one branch

--- src/main/java/survivalblock/dirtconduit/mixin/ConduitBlockEntityMixin.java
+++ src/main/java/survivalblock/dirtconduit/mixin/ConduitBlockEntityMixin.java
@@ -1,20 +1,30 @@
 package survivalblock.dirtconduit.mixin;
 
-import com.google.common.collect.Iterables;
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraft.block.Block;
 import net.minecraft.block.entity.ConduitBlockEntity;
-import net.minecraft.util.registry.Registry;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
 
+//@Debug(export = true)
 @Mixin(ConduitBlockEntity.class)
 public class ConduitBlockEntityMixin {
-	@Shadow @Final @Mutable
-	private static Block[] ACTIVATING_BLOCKS;
 
-	static {
-        ACTIVATING_BLOCKS = Iterables.toArray(Registry.BLOCK, Block.class);
-	}
+    @ModifyExpressionValue(method = "updateActivatingBlocks", at = @At(value = "FIELD", target = "Lnet/minecraft/block/entity/ConduitBlockEntity;ACTIVATING_BLOCKS:[Lnet/minecraft/block/Block;"))
+    private Block[] onlyOne(Block[] original) {
+        return new Block[]{original[0]};
+    }
+
+    @Definition(id = "getBlock", method = "Lnet/minecraft/block/BlockState;getBlock()Lnet/minecraft/block/Block;")
+    @Definition(id = "block", local = @Local(type = Block.class))
+    @Expression("?.getBlock() == block")
+    @WrapOperation(method = "updateActivatingBlocks", at = @At("MIXINEXTRAS:EXPRESSION"))
+    private boolean alwaysConfirm(Object left, Object right, Operation<Boolean> original) {
+        return true;
+    }
 }
